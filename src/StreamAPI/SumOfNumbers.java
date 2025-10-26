7c9718f7a05e4e4dfca537e6f998091e8f7ab836
Jitesh Singh <jiteshs101@gmail.com>
2025-10-26 11:19:33 +0530

    Sum of all numbers in a list

---
+++ src/StreamAPI/SumOfNumbers.java
@@ -0,0 +1,39 @@
+package StreamAPI;
+
+import java.util.List;
+
+//  Sum of all numbers in a list
+public class SumOfNumbers {
+
+    public static void main(String[] args) {
+
+        List<Integer> nums = List.of(1, 2, 3, 4, 5);
+
+        int sum = nums.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        System.out.println(sum);
+
+        int sum2 = nums.stream()
+                .mapToInt(Integer::valueOf)
+                .sum();
+
+        System.out.println(sum2);
+
+        int sum3 = nums.stream()
+                .reduce((accu, val) -> accu + val)
+                .get();
+
+        System.out.println(sum3);
+
+        int sum4 = nums.stream()
+                .reduce(0, (accu, val) -> accu + val);
+
+        System.out.println(sum4);
+
+    }
+}
+
+
+
