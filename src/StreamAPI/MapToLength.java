ad396979734e6b70a83a0dfead8e85735808fc32
Jitesh Singh <jiteshs101@gmail.com>
2026-01-08 19:08:52 +0530

    Map to length of strings

---
+++ src/StreamAPI/MapToLength.java
@@ -0,0 +1,23 @@
+package StreamAPI;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+// Map to length of strings
+public class MapToLength {
+
+    public static void main(String[] args) {
+        List<String> fruits = List.of("apple", "banana", "apricot", "kiwi", "orange", "lemon", "grapes", "papaya", "berry", "pomegranate", "pineapple", "mango");
+
+        Map<String, Integer> lengthMap =
+                fruits
+                        .stream()
+                        .collect(Collectors.toMap((str) -> str, (str) -> str.length()));
+
+        lengthMap.forEach((key, val) -> {
+            System.out.println("Key = " + key + " :: " + "Length = " + val);
+        });
+    }
+
+}
