f1d1f97b1da70bc72bdf736796a8e0650c5f9afe
Jitesh Singh <jiteshs101@gmail.com>
2026-01-14 20:55:02 +0530

    All distinct chars from strings

---
+++ src/StreamAPI/AllDistinctCharsFromStrings.java
@@ -0,0 +1,25 @@
+package StreamAPI;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+// Find max string by length
+
+public class AllDistinctCharsFromStrings {
+
+    public static void main(String[] args) {
+
+        List<String> names = Arrays.asList("Alice", "Bob", "Charlie", "David", "Eva", "Vandu", "Avinya", "Aaryash");
+
+        Set<Character> chars = names.stream()
+                .flatMap(s -> s.chars().mapToObj(c -> (char) c))
+                .collect(Collectors.toSet());
+
+        System.out.println(chars);
+    }
+
+}
