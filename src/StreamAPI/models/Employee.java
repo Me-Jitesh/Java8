ee2330bc74b727c02a55e445ba9b0ab6919f07bb
Jitesh Singh <jiteshs101@gmail.com>
2026-03-11 19:28:25 +0530

    Find total salary

--- src/StreamAPI/models/Employee.java
+++ src/StreamAPI/models/Employee.java
@@ -2,13 +2,37 @@ package StreamAPI.models;
 
 public class Employee {
 
-String name;
-String dept;
-Integer salary;
+    private String name;
+    private String dept;
+    private Double salary;
 
-    public Employee(String dept, String name,Integer salary) {
+    public Employee(String name, String dept, Double salary) {
+        this.name = name;
         this.dept = dept;
         this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
     }
 }
