6a15bf23d8fc96c5a78866ca89b9817287bf3373
JITESH SINGH <76907429+Me-Jitesh@users.noreply.github.com>
2026-03-28 16:46:00 +0530

    Convert list of employees to Map (name->dept)

---
+++ src/StreamAPI/ConvertListOfEmployeesToMap.java
@@ -0,0 +1,31 @@
+package StreamAPI;
+
+import StreamAPI.models.Employee;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+public class FindEmployeeCountByDepartment {
+
+    public static void main(String[] args) {
+        List<Employee> employees = List.of(
+                new Employee("Alice", "Engineering", 90000D),
+                new Employee("Bob", "Marketing", 60000D),
+                new Employee("Charlie", "Engineering", 85000D),
+                new Employee("Diana", "Marketing", 70000D),
+                new Employee("Eve", "HR", 50000D)
+        );
+
+        Map<String, String> map =
+                employees.stream()
+                        .collect(Collectors.toMap(
+                                Employee::getName,
+                                Employee::getDept
+                        ));
+
+        System.out.println(map);
+    }
+
+}
