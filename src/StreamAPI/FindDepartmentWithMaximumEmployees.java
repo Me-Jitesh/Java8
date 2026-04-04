d7e2bd135545990a439f55637e38acf60f61b77c
Jitesh Singh <jiteshs101@gmail.com>
2026-04-04 14:44:38 +0530

    Find department with maximum employees

---
+++ src/StreamAPI/FindDepartmentWithMaximumEmployees.java
@@ -0,0 +1,28 @@
+package StreamAPI;
+
+import StreamAPI.models.Employee;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class FindDepartmentWithMaximumEmployees {
+    public static void main(String[] args) {
+        List<Employee> employees = List.of(
+                new Employee("Alice", "Engineering", 90000D),
+                new Employee("Bob", "Marketing", 60000D),
+                new Employee("Charlie", "Engineering", 85000D),
+                new Employee("Diana", "Marketing", 70000D),
+                new Employee("Eve", "HR", 50000D)
+        );
+
+        String dept =
+                employees.stream()
+                        .collect(Collectors.groupingBy(Employee::getDept, Collectors.counting()))
+                        .entrySet().stream()
+                        .max(Map.Entry.comparingByValue())
+                        .get().getKey();
+
+        System.out.println(dept);
+    }
+}
