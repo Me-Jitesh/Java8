758aeb6ce4178e7491d2981588f11bb3298bce1f
Jitesh Singh <jiteshs101@gmail.com>
2026-04-13 16:16:33 +0530

    Find top 2 highest paid employees per department

---
+++ src/StreamAPI/FindTop2HighestPaidEmployeesPerDepartment.java
@@ -0,0 +1,32 @@
+package StreamAPI;
+
+import StreamAPI.data.EmployeeDB;
+import StreamAPI.models.Employee;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class FindTop2HighestPaidEmployeesPerDepartment {
+
+    public static void main(String[] args) {
+
+        List<Employee> employees = EmployeeDB.getEmployees();
+
+        Map<String, List<Employee>> result =
+                employees.stream()
+                        .collect(Collectors.groupingBy(
+                                Employee::getDept,
+                                Collectors.collectingAndThen(
+                                        Collectors.toList(),
+                                        list -> list.stream()
+                                                .sorted(Comparator.comparing(Employee::getSalary).reversed())
+                                                .limit(2)
+                                                .collect(Collectors.toList())
+                                )
+                        ));
+
+        System.out.println(result);
+    }
+}
